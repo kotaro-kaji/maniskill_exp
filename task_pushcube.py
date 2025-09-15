4c2e78cbb077351177566a01ff010096c432e181
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-09-15 13:20:31 +0900

    comment out about rgb input

--- task_pushcube.py
+++ task_pushcube.py
@@ -51,21 +51,21 @@ class MyPushCubeEnv(BaseEnv):
         base_pose = sapien.Pose(p=[0.0, 0.0, 0.0])
         super()._load_agent(options, base_pose)
 
-    @property
-    def _default_sensor_configs(self):
-        # トレーニング用の小さめカメラ（高速）
-        pose = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
-        return [
-            CameraConfig(
-                "base_camera",
-                pose=pose,
-                width=128,
-                height=128,
-                fov=np.pi / 2,
-                near=0.01,
-                far=100,
-            )
-        ]
+    #@property
+    #def _default_sensor_configs(self):
+        ## トレーニング用の小さめカメラ（高速）
+        #pose = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
+        #return [
+            #CameraConfig(
+                #"base_camera",
+                #pose=pose,
+                #width=128,
+                #height=128,
+                #fov=np.pi / 2,
+                #near=0.01,
+                #far=100,
+            #)
+        #]
 
     @property
     def _default_human_render_camera_configs(self):
