af884e3e52dd4237ec2d3360c807a73e8c9a83a3
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-09-15 19:10:56 +0900

    test: extend GPU test loop

--- test_gpu.py
+++ test_gpu.py
@@ -1,5 +1,6 @@
 import my_xarm7  # imports your robot and registers it
 import my_xarm7_wo_gripper
+import my_xarm7_mjcf
 # imports the demo_robot example script and lets you test your new robot
 import mani_skill.examples.demo_robot as demo_robot_script
 
