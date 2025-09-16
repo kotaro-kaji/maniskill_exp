4646301e4b58ddc34fd0027070a049e040795568
jika_ubuntu_on_ThinkPad_L13_Gen2 <jika1366119@gmail.com>
2025-09-16 13:56:17 +0900

    fixed initial pose

--- my_xarm7_mjcf_1.py
+++ my_xarm7_mjcf_1.py
@@ -115,14 +115,15 @@ class Xarm7MJCFv1(BaseAgent):
             "right_finger_joint": {"joint": "left_driver_joint"},
         }
 
+        # Invert user-level mapping so that normalized +1 -> closed (q=0), -1 -> open (q=0.85)
         gripper_abs = PDJointPosMimicControllerConfig(
             self.gripper_joint_names,
-            0.0,
             0.85,
+            0.0,
             self.gripper_stiffness,
             self.gripper_damping,
             self.gripper_force_limit,
-            normalize_action=True,  # [-1,1] -> [0,0.8]
+            normalize_action=True,  # [-1,1] maps to [0.85..0.0]
             interpolate=True,
         )
         gripper_abs.mimic = mimic_map
