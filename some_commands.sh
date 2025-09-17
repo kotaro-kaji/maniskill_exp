d4ba4b2ebe8060f92581f4abb6605b4ea31d0274
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-09-17 13:10:10 +0900

    added some commands

--- some_commands.sh
+++ some_commands.sh
@@ -27,6 +27,12 @@ python ppo_xarm7.py \
   --track False \
   --sim-backend cpu \
   --exp-name "smoke-ppo-xarm7-pushcube-cpu"
+
+
+
+
+
+
 =======
 # 確認コマンド
 python ppo_xarm7.py --env_id MyPushCube-v1 --seed 0 --num_envs 64 --num-steps 4 --update_epochs 2 --num_minibatches 8 --total_timesteps 256 --exp-name "smoke-ppo-xarm7-pushcube"
@@ -36,6 +42,8 @@ _envs 64 --num-steps 4 --update_epochs 2 --num_minibatches 8 --total_timesteps 2
 
 # ガチの学習
 python ppo_xarm7.py --env_id MyPushCube-v1 --seed 42 --num_envs 1024 --num-steps 8 --update_epochs 8 --num_minibatches 32 --total_timesteps 50_000_000 --num_eval_envs 16 --control-mode pd_joint_delta_pos --exp-name "ppo-MyPushCube-v1-xarm7-42"
-
+python ppo_xarm7.py --env_id="MyPushCube-v1" \
+  --control-mode pd_joint_delta_pos --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
+  --total_timesteps=25_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99 
 # ガチの学習
 python ppo_xarm7_mjcf.py --env_id MyPushCube-v1 --seed 42 --num_envs 1024 --num-steps 8 --update_epochs 8 --num_minibatches 32 --total_timesteps 50_000_000 --num_eval_envs 16 --control-mode pd_joint_delta_pos --exp-name "ppo-MyPushCube-v1-xarm7-42"
