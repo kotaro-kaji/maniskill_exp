560f2a226d0d3336f1362a75cc385536d232f772
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-09-19 11:18:26 +0900

    condcut some experiments

--- ppo_xarm7.py
+++ ppo_xarm7.py
@@ -48,7 +48,7 @@ class Args:
     """if toggled, only runs evaluation with the given model checkpoint and saves the evaluation trajectories"""
     checkpoint: Optional[str] = None
     """path to a pretrained checkpoint file to start evaluation/training from"""
-    print_eval_actions: bool = False
+    print_eval_actions: bool = True
     """if toggled, prints the actions issued during evaluation"""
 
     # Algorithm specific arguments
