834a19de52dab093459fdf13808fee1563050174
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-09-23 22:19:53 +0900

    tiny change

--- ppo_rollout_xarm7.py
+++ ppo_rollout_xarm7.py
@@ -19,7 +19,7 @@ from mani_skill.utils.wrappers.record import RecordEpisode
 from mani_skill.vector.wrappers.gymnasium import ManiSkillVectorEnv
 
 from ppo_xarm7 import Agent
-
+from task_joint_hold import MyJointHoldEnv
 
 CSV_LOG_FILENAME = "rollout_log.csv"
 INFO_LOG_FILENAME = "rollout_info.jsonl"
