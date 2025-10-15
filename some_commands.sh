39bd6367ced1cd43231780d1e8f15ae5ee34e0a6
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-10-15 15:33:24 +0900

    ok

--- some_commands.sh
+++ some_commands.sh
@@ -58,8 +58,9 @@ python ppo_xarm7.py --env_id="MyJointHold-v0" \
 
 
 python ppo_xarm7.py --env_id="MyEEAlignMarker-v0" \
-  --control-mode sdk_joint_delta_pos --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
-  --total_timesteps=25_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99 --print_eval_actions
+  --control-mode rate_limited_pd_joint_delta_pos --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
+  --total_timesteps=100_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99 --print_eval_actions
+
 
 
 
