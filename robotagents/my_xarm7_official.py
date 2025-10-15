4bda789df9d84f5a5896d189a444f8927f682e81
Kotaro Kajitsuka <215938109+kotaro-kaji@users.noreply.github.com>
2025-10-15 16:14:35 +0900

    tune(controller): add xArm jerk limit

--- robotagents/my_xarm7_official.py
+++ robotagents/my_xarm7_official.py
@@ -66,6 +66,7 @@ class Xarm7Official(Xarm7):
             use_target=False,
             max_velocity=_broadcast(np.deg2rad(60.0), arm_dof),
             max_acceleration=_broadcast(np.deg2rad(600.0), arm_dof),
+            max_jerk=_broadcast(np.deg2rad(5000.0), arm_dof),
         )
 
         controller_configs = OrderedDict()
