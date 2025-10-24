a940964b84fc1687b7f8903e285d02f87821873e
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-10-24 10:20:59 +0900

    tune(pushcube): reduce episode horizon

--- task_pushcube_beatiful.py
+++ task_pushcube_beatiful.py
@@ -36,7 +36,7 @@ import robotagents.my_xarm7_mjcf  # registers Xarm7MJCF (uid: "my_xarm7_mjcf")
 # from mani_skill.agents.robots import Fetch, Panda
 
 
-@register_env("MyPushCube-v1", max_episode_steps=300)
+@register_env("MyPushCube-v1", max_episode_steps=100)
 class MyPushCubeEnv(BaseEnv):
 
     goal_radius = 0.1
