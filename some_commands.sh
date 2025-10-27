9af918bec170f96fa5583d2a9668130a0b5d026e
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-10-27 10:11:45 +0900

    tiny change

--- some_commands.sh
+++ some_commands.sh
@@ -83,7 +83,7 @@ python file_control_xarm7_delta.py --env_id MySimpleReach-v0 --control-mode rate
 
 python ppo_xarm7.py --env_id="MyEEAlignMarker-v0"   --control-mode rate_limited_pd_joint_delta_pos --num_envs=1024 --update_epochs=8 --num_minibatches=32   --total_timesteps=100_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99 --print_eval_actions
 
-python ppo_xarm7.py --env_id="MyEEAlignMarker-v0"   --control-mode pd_joint_delta_pos --num_envs=1024 --update_epochs=8 --num_minibatches=32   --total_timesteps=100_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99 --print_eval_actions
+python ppo_xarm7.py --env_id="MyEEAlignMarker-v0"   --control-mode pd_joint_delta_pos --num_envs=1024 --update_epochs=8 --num_minibatches=32   --total_timesteps=50_000_000 --num-steps=200 --num_eval_steps=200 --gamma=0.99
 
 python ppo_xarm7.py --env_id="MyEEAlignMarker-v0"   --control-mode official_pd_joint_delta_pos --num_envs=1024 --update_epochs=8 --num_minibatches=32   --total_timesteps=100_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99 --print_eval_actions -checkpoint runs/MyEEAlignMarker-v0__ppo_xarm7__1__1760590273/ckpt_76.pt
 
