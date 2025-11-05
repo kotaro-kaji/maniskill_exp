e16158f9f3f4b66604cebeec1dd2f8b5aee64e53
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-11-05 13:36:39 +0900

    ok

--- robotagents/my_xarm7.py
+++ robotagents/my_xarm7.py
@@ -164,8 +164,8 @@ class Xarm7(BaseAgent):
         )
         arm_pd_joint_delta_pos = PDJointPosControllerConfig(
             self.arm_joint_names,
-            lower = -0.02,
-            upper = 0.02,
+            lower = -0.10,
+            upper = 0.10,
             stiffness = self.arm_stiffness,
             damping =  self.arm_damping,
             force_limit = self.arm_force_limit,
