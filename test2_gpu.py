364bb7285614b20fef309c8c150da31a479b6d75
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-11-05 15:11:09 +0900

    chore: switch GPU visualizer to TwoRobotPickCube

--- test2_gpu.py
+++ test2_gpu.py
@@ -5,11 +5,8 @@ import task_pushcube_beatiful
 import tasks.du.task_dual_simple
 
 #env_id = "MyPushCube-v1"
-env_id = "MyDualSimple-v0"
-
-print(f"Environment ID: {env_id}")
-print(f"Observation space: {env.observation_space}")
-print(f"Action space: {env.action_space}")
+#env_id = "MyDualSimple-v0"
+env_id = "TwoRobotPickCube-v1"
 
 env = gym.make(
     env_id,
@@ -17,12 +14,19 @@ env = gym.make(
     control_mode="pd_joint_delta_pos",
     render_mode="human"
 )
+
+print(f"Environment ID: {env_id}")
+print(f"Observation space: {env.observation_space}")
+print(f"Action space: {env.action_space}")
+
+
 obs, _ = env.reset(seed=0)
 done = False
 while True:
     action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
-    print(f"Step: {env.num_steps}, Reward: {reward}, Terminated: {terminated}, Truncated: {truncated}")
+    #print(f"Step: {env.num_steps}")
+    #print(f"Reward: {reward}, Terminated: {terminated}, Truncated: {truncated}")
     done = terminated or truncated
     env.render()  # GUIウィンドウ表示
 print(f"Episode finished: {info}")
