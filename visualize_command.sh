1e92fe426faaad3505c9781c1efb3ab052b4672c
Kotaro Kajitsuka <215938109+kotaro-kaji@users.noreply.github.com>
2025-12-01 08:26:10 +0900

    refactor: Update gripper collision models and box dimensions - Replaced STL meshes with box collision models for gripper fingers - Adjusted box dimensions and offset for better simulation accuracy - Updated stick attachment point and end effector sphere size - Added visualization script for URDF - Cleaned up imports in test script

---
+++ visualize_command.sh
@@ -0,0 +1,13 @@
+#1つ目のターミナル
+source ~/ros2_ws/install/setup.bash ;
+ros2 run robot_state_publisher robot_state_publisher   --ros-args -p robot_description:="$(cat /home/jika/Desktop/my_projects/robot/maniskill_exp/xarm7_1305_left_ball_ee.urdf)"
+
+#2つ目のターミナル
+source ~/ros2_ws/install/setup.bash ;
+ros2 run joint_state_publisher_gui joint_state_publisher_gui   --ros-args -p robot_description:="$(cat /home/jika/Desktop/my_projects/robot/maniskill_exp/xarm7_1305_left_ball_ee.urdf)"
+
+#3つ目のターミナル
+source ~/ros2_ws/install/setup.bash ;
+ ros2 run rviz2 rviz2 -d ~/ros2_ws/src/xarm_ros2/xarm_description/rviz/display.rviz
+
+
