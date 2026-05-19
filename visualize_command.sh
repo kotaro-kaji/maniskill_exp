2234580ec1d3a0479c9d736fb6d42b90274ac440
kotaro-kaji <215938109+kotaro-kaji@users.noreply.github.com>
2026-05-19 15:21:24 +0900

    chore: add rviz panda visualization urdf

--- visualize_command.sh
+++ visualize_command.sh
@@ -12,17 +12,15 @@ ros2 run rviz2 rviz2 -d ~/ros2_ws/src/xarm_ros2/xarm_description/rviz/display.rv
 
 
 
-
+# pandaの可視化
 #1つ目のターミナル
 source ~/ros2_ws/install/setup.bash ;
-ros2 run robot_state_publisher robot_state_publisher   --ros-args -p robot_description:="$(cat /home/kotaro/my_projects/robot/maniskill_exp/xarm7_1305_left_ball_ee.urdf)"
+ros2 run robot_state_publisher robot_state_publisher   --ros-args -p robot_description:="$(cat /home/kotaro/my_projects/robotics/maniskill_exp/robotagents/assets/panda/panda_v2_rviz.urdf)"
 
 #2つ目のターミナル
 source ~/ros2_ws/install/setup.bash ;
-ros2 run joint_state_publisher_gui joint_state_publisher_gui   --ros-args -p robot_description:="$(cat /home/kotaro/my_projects/robot/maniskill_exp/xarm7_1305_left_ball_ee.urdf)"
+ros2 run joint_state_publisher_gui joint_state_publisher_gui   --ros-args -p robot_description:="$(cat /home/kotaro/my_projects/robotics/maniskill_exp/robotagents/assets/panda/panda_v2_rviz.urdf)"
 
 #3つ目のターミナル
 source ~/ros2_ws/install/setup.bash ;
 ros2 run rviz2 rviz2 -d ~/ros2_ws/src/xarm_ros2/xarm_description/rviz/display.rviz
-
-
