a2405c8fb009d6a897db122207f43a74cd29e473
kotaro-kaji <215938109+kotaro-kaji@users.noreply.github.com>
2026-06-11 10:58:27 +0900

    fix: update kinematics gripper urdf joints

--- robotagents/my_xarm7.py
+++ robotagents/my_xarm7.py
@@ -14,7 +14,8 @@ from mani_skill.utils.structs.actor import Actor
 @register_agent()
 class Xarm7(BaseAgent):
     uid = "my_xarm7"
-    urdf_path = "robotagents/assets/xarm7/xarm7_1305_gripper_realsense_sn_kinematics.urdf"
+    #urdf_path = "robotagents/assets/xarm7/xarm7_1305_gripper_realsense_sn_kinematics.urdf"
+    urdf_path = "robotagents/assets/xarm7/xarm7_1305_left.urdf"
     urdf_config = dict(
         _materials=dict(
             gripper=dict(static_friction=2.0, dynamic_friction=2.0, restitution=0.0)
